a8620c8794a20e6fe2ce51da957853092d685d82
Mrcsbda <macbedoya@gmail.com>
2023-01-12 14:08:57 -0500

    encrypter

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
-const noEncrypted = document.getElementById('no-encrypted');
-const encrypted = document.getElementById('encrypted');
+const noEncrypted = document.getElementById("no-encrypted");
+const encrypted = document.getElementById("encrypted");
+const message = document.getElementById("message");
+const result = document.getElementById("result");
 
 function encrypt() {
-  noEncrypted.classList.remove('no-encrypted-active');
-  encrypted.classList.add('encrypted-active')
+  console.log(this.message.value.toLowerCase());
+  if (this.message.value.length) {
+    this.result.innerHTML = this.message.value
+      .toLowerCase()
+      .split("")
+      .map((letter) => change(letter))
+      .toString()
+      .replaceAll(",", "");
+  } else {
+    this.result.innerHTML = "No has introducido ningún mensaje.";
+  }
+
+  noEncrypted.classList.remove("no-encrypted-active");
+  encrypted.classList.add("encrypted-active");
+}
+
+function change(letter) {
+  switch (letter) {
+    case "a":
+      return "ai";
+    case "e":
+      return "enter";
+    case "i":
+      return "imes";
+    case "o":
+      return "ober";
+    case "u":
+      return "ufat";
+    default:
+      return letter;
+  }
 }
 
 function decrypt() {
-    noEncrypted.classList.remove('no-encrypted-active');
-    encrypted.classList.add('encrypted-active')
+  noEncrypted.classList.remove("no-encrypted-active");
+  encrypted.classList.add("encrypted-active");
 }
